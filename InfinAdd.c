56574ccbbe5448534be2dd6affd8e81e259b58e4
Julien Moussaoui <JulienMOBA@localhost.localdomain>
2020-10-26 06:43:19 -0400

    sending the work

--- InfinAdd.c
+++ InfinAdd.c
@@ -5,12 +5,15 @@
 ** infinite addition
 */
 
-int my_strlen (char const *str)
-    void integer_count()
+int my_strlen (char const *str);
+void integer_count(char *str_one, char const *str_two)
 {
-    int i = 0;
-    int j = 0;
+    int i;
+    int j;
 
+    while (str_one[i] || str_two[j]) {
+        k++;
+    }
     while (i < my_strlen(str_one) - 1) {
         i++;
     }
@@ -18,7 +21,7 @@ int my_strlen (char const *str)
         j++;
     }
 }
-    
+
 char calcul(char *str_one, char *str_two)
 {
     int i = 0;
@@ -26,11 +29,13 @@ char calcul(char *str_one, char *str_two)
     int k = 0;
     char *dest malloc(sizeof (char) * (my_strlen(str_one) + my_strlen(str_two)));
 
+    integer_count(str_one, str_two);
+    
     while (str_one[i] || str_two[j]) {
-        dest = (str_one[i] + str_two[j]) - 48;
-        if (dest[k] > '9'){
+        dest[k] = (str_one[i] + str_two[j]) - 48;
+        if (dest[k] > '9') {
             dest[k] = dest[k] - 58;
-            str_one[i - 1] += 1;
+            dest[k - 1] += 1;
         }
         i--;
         j--;
@@ -38,17 +43,22 @@ char calcul(char *str_one, char *str_two)
     }
 }
 
+void check_string(char *str_one, char *str_two) {
+    int h = 0;
+    int l = 0;
+
+    while (str_one[h]) {
+        if (str_one[0] == '-' || str_one[h] >= '0' && str_one[h] <= '9')
+            h++;
+    }
+    while (str_two[l]) {
+        if (str_two[0] == '-' || str_two[l] >= '0' && str_two[l] <= '9')
+            l++;
+    }
+}
+
 
 char *infinadd(char *str_one, char *str_two)
 {
-    int i = 0;
-    int j = 0;
-
-    while(str_one[i] ||str_two[j]) {
-        if (str_one[0] == '-' || str_one[i] >= '0' && str_one[i] <= '9')
-            i++;
-        if (str_two[0] == '-' || str_two[j] >= '0' && str_two[j] <= '9')
-            j++;
-
-    }
+    
 }
