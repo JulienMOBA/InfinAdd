c6ccdd18fa002ea50f586f5bed9a65d0f183943a
Julien Moussaoui <JulienMOBA@localhost.localdomain>
2020-10-26 04:30:26 -0400

    yessay

---
+++ InfinAdd.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** InfinAdd
+** File description:
+** infinite addition
+*/
+
+char *infinadd(char *str_one, char *str_two)
+{
+    int i = 0;
+    
+    while(str_one[])
+}
